8f517159d4115bdf4d6b2b68f3bd92662928b16a
WEBDEV-Rajat <162951475+WEBDEV-Rajat@users.noreply.github.com>
2024-07-01 23:42:34 +0530

    Added changes in Title

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,7 @@ import { ShoppingCart } from 'phosphor-react';
 export const Navbar = () => {
   return (
     <div className="navbar">
+      <h1>THRIFTIFY</h1>
         <div className="links">
         <Link to= "/"> Shop </Link>
         <Link to= "/cart"><ShoppingCart size = {32} /></Link>
